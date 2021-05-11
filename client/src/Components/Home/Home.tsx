7000c35291e34e15c106aa01144a4a3bb05a334c
Juan Ruiz <juancaruiz@Juancas-MacBook-Pro.local>
2021-05-11 14:40:25 -0700

    built home page schema

--- client/src/Components/Home/Home.tsx
+++ client/src/Components/Home/Home.tsx
@@ -27,29 +27,29 @@ function dateChanger(day: number): string {
 }
 
 function monthChanger(month: number): string {
-  if (month === 1) {
+  if (month === 0) {
     return "Jan";
-  } else if (month === 2) {
+  } else if (month === 1) {
     return "Feb";
-  } else if (month === 3) {
+  } else if (month === 2) {
     return "March";
-  } else if (month === 4) {
+  } else if (month === 3) {
     return "April";
-  } else if (month === 5) {
+  } else if (month === 4) {
     return "May";
-  } else if (month === 6) {
+  } else if (month === 5) {
     return "June";
-  } else if (month === 7) {
+  } else if (month === 6) {
     return "July";
-  } else if (month === 8) {
+  } else if (month === 7) {
     return "Aug";
+  } else if (month === 8) {
+    return "Sep";
   } else if (month === 9) {
-    return "Sept";
-  } else if (month === 10) {
     return "Oct";
-  } else if (month === 11) {
+  } else if (month === 10) {
     return "Nov";
-  } else if (month === 12) {
+  } else if (month === 11) {
     return "Dec";
   }
   return monthChanger(month);
